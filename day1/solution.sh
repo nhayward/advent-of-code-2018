5a990466b4779dae11638c037036c2c15dca398a
Nick Hayward <nhayward2011@gmail.com>
2018-12-07 00:20:11 -0500

    day 1 part 2

--- day1/solution.sh
+++ day1/solution.sh
@@ -1,6 +1,32 @@
-expression="0"
-while read change
-do
-  expression="$expression$change"
+changes=()
+while read change; do
+  changes+=( "$change" )
 done < "input.txt"
-echo "$(($expression))"
+total="0"
+frequencies=("0")
+totalFound=false
+firstRepeatFound=false
+for ((i=0; i < ${#changes[@]}; i++)); do
+  total="$(($total${changes[i]}))"
+  for item in ${frequencies[@]}; do
+    if [ $item = $total ]; then
+      echo "First repeat frequency: $total"
+      firstRepeatFound=true
+      if [ totalFound = true ]; then
+        break 3
+      else
+        break 2
+      fi
+    fi
+  done
+  frequencies+=("$total")
+  if [ $i = $((${#changes[@]}-1)) ]; then
+    if [ $totalFound = false ]; then
+      echo "Total: $total"
+      totalFound=true
+    fi
+    if [ $firstRepeatFound = false ]; then
+      i=0
+    fi
+  fi
+done
