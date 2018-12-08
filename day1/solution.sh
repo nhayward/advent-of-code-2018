700250997a792de72b7eb4ab8a9ff423db637bed
Nick Hayward <nhayward2011@gmail.com>
2018-12-08 17:41:33 -0500

    day 1 part 2 better solution

--- day1/solution.sh
+++ day1/solution.sh
@@ -1,30 +1,29 @@
+#!/bin/bash
 changes=()
 while read change; do
   changes+=( "$change" )
 done < "input.txt"
 total="0"
-frequencies=("0")
+declare -A frequencies
+frequencies=(["0"]=true)
 totalFound=false
 firstRepeatFound=false
 for ((i=0; i < ${#changes[@]}; i++)); do
   total="$(($total${changes[i]}))"
   if [ $firstRepeatFound = false ]; then
-    for item in ${frequencies[@]}; do
-      if [ $item = $total ]; then
-        echo "First repeat frequency: $total"
-        firstRepeatFound=true
-        break
-      fi
-    done
+    if [ ${frequencies["$total"]} ]; then
+      echo "First repeat frequency: $total"
+      firstRepeatFound=true
+      break
+    fi
   fi
-  frequencies+=("$total")
+  frequencies["$total"]+=true
   if [ $i = $((${#changes[@]}-1)) ]; then
     if [ $totalFound = false ]; then
       echo "Total: $total"
       totalFound=true
     fi
     if [ $firstRepeatFound = false ]; then
-      echo "repeating"
       i=-1
     fi
   fi
